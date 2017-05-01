41b207245607d58c57200579a525757b9154ee0c
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-05-01 23:29:26 +0530

    Navigation ViewController

---
+++ Navigation View Controller/NavigationViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationViewController
+//
+//  Created by OSX on 09/07/16.
+//  Copyright © 2016 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
